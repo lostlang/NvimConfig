3e44ac11fdcbd60bcd9ab42d3bf79ac8f931aac6
Alec Lang <lostlang@icloud.com>
2022-11-09 03:34:23 +0300

    v1.7 Add file name

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -11,7 +11,7 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff'},
+    lualine_b = {'branch', 'filename', 'diff'},
     lualine_c = {'diagnostics'},
     lualine_x = {'filetype'},
     lualine_y = {'encoding'},
