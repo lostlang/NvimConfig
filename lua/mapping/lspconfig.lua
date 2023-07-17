336b7a69942c473a615a2b6d90d5a54bd4a29745
Alec Lang <lostlang@icloud.com>
2023-07-17 13:06:52 +0300

    feat: Add keymap

--- lua/mapping/lspconfig.lua
+++ lua/mapping/lspconfig.lua
@@ -1,4 +1,5 @@
 Map("n", "gD", Vim.lsp.buf.declaration, { silent = true })
 Map("n", "gd", Vim.lsp.buf.definition, { silent = true })
-Map("n", "K", Vim.lsp.buf.hover, { silent = true })
+Map("n", "gr", Vim.lsp.buf.rename, { silent = true })
+Map("n", "go", Vim.lsp.buf.hover, { silent = true })
 Map("n", "gi", Vim.lsp.buf.implementation, { silent = true })
